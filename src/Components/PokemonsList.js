dc95e010995ac5b85515a0ad604ba6676c4cf985
Mitzi17 <m_estrada17@live.com>
2023-07-14 20:07:53 -0700

    imported Pokemon and passed down pokemon object and method updatePokemon

--- src/Components/PokemonsList.js
+++ src/Components/PokemonsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { pokemonsApi } from '../Api/PokemonsApi';
-
+import Pokemon from './Pokemon';
 
 export default class PokemonsList extends React.Component {
     state = {
@@ -13,13 +13,26 @@ export default class PokemonsList extends React.Component {
 
     fetchPokemons = async () => {
         const pokemons = await pokemonsApi.get();
-    }
+        this.setState({ pokemons });
+    };
+
+    updatePokemon = async (updatePokemon) => {
+        await pokemonsApi.put(updatePokemon);
+        this.fetchPokemons();
+    };
 
     render() {
         return (
-            <div>
-
+            <div className='pokemon-list'>
+                {this.state.pokemons.map((pokemon) => (
+                    <Pokemon
+                        pokemon={pokemon}
+                        key={pokemon.id}
+                        updatePokemon={this.updatePokemon}
+                    />
+                ))}
             </div>
         )
     }
+
 }
