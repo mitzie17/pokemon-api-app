7b39866047b058b87de3685c635781699db1401a
Mitzi17 <m_estrada17@live.com>
2023-08-27 20:59:55 -0700

    created file for homepage

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
+
+export const Home = () => {
+  return (
+    <div className="home-div">
+      Home
+      <Image
+        src="https://mir-s3-cdn-cf.behance.net/projects/404/82d2c5174192145.Y3JvcCw5MTYsNzE3LDIwLDA.png"
+        roundedCircle
+      />
+    </div>
+  );
+};
