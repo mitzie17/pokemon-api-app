190fd5a2dce2f101993ad82f252fba9d96698b95
Mitzi17 <m_estrada17@live.com>
2023-08-28 22:51:02 -0700

    card styling changes

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,52 +1,79 @@
 import React from "react";
 import { NewAbilityForm } from "./NewAbilityForm";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 
 export const Pokemon = (props) => {
+  const { pokemon, updatePokemon } = props;
 
-    const { pokemon, updatePokemon } = props;
-
-    // Here we pass in the id of the ability to be removed.
-    const deleteAbility = (abilityId) => {
+  // Here we pass in the id of the ability to be removed.
+  const deleteAbility = (abilityId) => {
     // The updatedPokemon variable holds the pokemon object that has the ability we want to remove.
-        const updatedPokemon = {
-    // The pokemon object's abilities array is updated by filtering out the ability we want to remove.
-            ...pokemon,
-            abilities: pokemon.abilities.filter((x) => x !== abilityId)
-        };
+    const updatedPokemon = {
+      // The pokemon object's abilities array is updated by filtering out the ability we want to remove.
+      ...pokemon,
+      abilities: pokemon.abilities.filter((x) => x !== abilityId),
+    };
     // Then the updatePokemon method (from the PokemonsList class) is called and the updatedPokemon object is passed in.
-        updatePokemon(updatedPokemon);
+    updatePokemon(updatedPokemon);
+  };
+
+  // Here we pass in an ability to the addNewAbility method
+  const addNewAbility = (ability) =>
+    updatePokemon({ ...pokemon, abilities: [...pokemon.abilities, ability] });
+  // This method returns the updatePokemon method, which takes in a pokemon object, and a new abilities array with the new ability included.
+
+  const abilities = () => (
+    <ListGroup className="list-group-flush">
+      {pokemon.abilities.map((ability, index) => (
+        <ListGroup.Item>
+          <div key={index}>
+            <label>{ability}</label>
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={(e) => deleteAbility(ability)}
+            >
+              Delete
+            </button>
+          </div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+
+  const bgColorChange = (pokemonType) => {
+    if (pokemon.type === "Water") {
+      return "primary";
+    } else if (pokemon.type === "Fire") {
+      return "danger";
+    } else if (pokemon.type === "Grass") {
+      return "success";
+    } else if (pokemon.type === "Electric") {
+      return "warning";
+    } else {
+      return "dark";
     }
+  };
 
-    // Here we pass in an ability to the addNewAbility method
-    const addNewAbility = (ability) => updatePokemon({ ...pokemon, abilities: [...pokemon.abilities, ability]})
-    // This method returns the updatePokemon method, which takes in a pokemon object, and a new abilities array with the new ability included.
-    
+  return (
+    <Card
+      style={{ width: "18rem" }}
+      className="mb-2"
+      bg={bgColorChange(pokemon.type)}
+    >
+      <Card.Header as="h1">
+        <Card.Title>{pokemon.name}</Card.Title>
+        <Card.Subtitle>Type: {pokemon.type}</Card.Subtitle>
+      </Card.Header>
+      <Card.Body>
+        <Card.Subtitle>Abilities:</Card.Subtitle>
+        <Card.Text>
+          {abilities({ abilities, pokemonId: pokemon.id, deleteAbility })}
 
-    const abilities = () => (
-        <ul>
-            {pokemon.abilities.map((ability, index) => (
-                <li key={index}>
-                    <label>{ability}</label>
-                    <button className='btn btn-danger btn-sm' onClick={(e) => deleteAbility(ability)}>Delete</button>
-                </li>
-            ))}
-        </ul>
-    );
-
-    return (
-        <div className="card">
-            <div className="card-header">
-                <h2 className="card-tile">{pokemon.name}</h2>
-                <h6>Type: {pokemon.type}</h6>
-            </div>
-            <div className="card-body">
-                {
-                abilities({abilities, pokemonId: pokemon.id, deleteAbility})
-                }
-                <NewAbilityForm addNewAbility={addNewAbility}/>
-            </div>
-            
-        </div>
-    );
-
+          <NewAbilityForm addNewAbility={addNewAbility} />
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
