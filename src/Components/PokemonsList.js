f7f93e77530b19fc6b06d93f4a432fe032134d4d
Mitzi17 <m_estrada17@live.com>
2023-09-01 22:50:42 -0700

    change card layout

--- src/Components/PokemonsList.js
+++ src/Components/PokemonsList.js
@@ -1,38 +1,43 @@
-import React from 'react';
-import { pokemonsApi } from '../Api/PokemonsApi';
-import { Pokemon } from './Pokemon';
+import React from "react";
+import { pokemonsApi } from "../Api/PokemonsApi";
+import { Pokemon } from "./Pokemon";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class PokemonsList extends React.Component {
-    state = {
-        pokemons: []
-    };
+  state = {
+    pokemons: [],
+  };
 
-    componentDidMount() {
-        this.fetchPokemons();
-    }
+  componentDidMount() {
+    this.fetchPokemons();
+  }
 
-    fetchPokemons = async () => {
-        const pokemons = await pokemonsApi.get();
-        this.setState({ pokemons });
-    };
+  fetchPokemons = async () => {
+    const pokemons = await pokemonsApi.get();
+    this.setState({ pokemons });
+  };
 
-    updatePokemon = async (updatePokemon) => {
-        await pokemonsApi.put(updatePokemon);
-        this.fetchPokemons();
-    };
+  updatePokemon = async (updatePokemon) => {
+    await pokemonsApi.put(updatePokemon);
+    this.fetchPokemons();
+  };
 
-    render() {
-        return (
-            <div className='pokemon-list'>
-                {this.state.pokemons.map((pokemon) => (
-                    <Pokemon
-                        pokemon={pokemon}
-                        key={pokemon.id}
-                        updatePokemon={this.updatePokemon}
-                    />
-                ))}
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="pokemon-list">
+        <Row sm={2} md={3}>
+          {this.state.pokemons.map((pokemon) => (
+            <Col>
+              <Pokemon
+                pokemon={pokemon}
+                key={pokemon.id}
+                updatePokemon={this.updatePokemon}
+              />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    );
+  }
 }
