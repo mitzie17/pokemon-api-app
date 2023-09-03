9e0dd557820c94ad5de1b7ddd9c3df645f67f1ca
Mitzi17 <m_estrada17@live.com>
2023-09-02 22:08:40 -0700

    card styling

--- src/Components/NewAbilityForm.js
+++ src/Components/NewAbilityForm.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const NewAbilityForm = (props) => {
-    const [ ability, setAbility ] = useState('');
+  const [ability, setAbility] = useState("");
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (ability) {
-            props.addNewAbility(ability);
-            setAbility('');
-        } else {
-            console.log('invalid input');
-        }
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (ability) {
+      props.addNewAbility(ability);
+      setAbility("");
+    } else {
+      console.log("invalid input");
+    }
+  };
 
-    return (
-        <div>
-            
-            <form onSubmit={onSubmit}>
-            <h5>Add a new ability</h5>
-                <div className='form-group'>
-                <input
-                    type='text'
-                    placeholder='ability name'
-                    onChange={(e) => setAbility(e.target.value)}
-                    value={ability}
-                />
-                <button className='btn btn-success btn-sm' type='submit'>Add new ability</button>
-                </div>
-                
-            </form>
+  return (
+    <div className="ability-form m-3">
+      <form onSubmit={onSubmit}>
+        <h6>Add a new ability:</h6>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="ability name"
+            onChange={(e) => setAbility(e.target.value)}
+            value={ability}
+          />
+          <button className="btn btn-dark btn-sm" type="submit">
+            Add new ability
+          </button>
         </div>
-    )
-
-}
+      </form>
+    </div>
+  );
+};
