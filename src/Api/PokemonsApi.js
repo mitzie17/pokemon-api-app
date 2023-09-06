a6629c122fca6fc309cc5ca700803ec2e7968870
Mitzi17 <m_estrada17@live.com>
2023-09-05 21:02:51 -0700

    added post api call to create new pokemon

--- src/Api/PokemonsApi.js
+++ src/Api/PokemonsApi.js
@@ -1,31 +1,50 @@
-const POKEMONS_API = 'https://64b0cb52062767bc48252eba.mockapi.io/pokemon';
+const POKEMONS_API = "https://64b0cb52062767bc48252eba.mockapi.io/pokemon";
 
 class PokemonsApi {
-    get = async () => {
-        try {
-            const resp = await fetch(POKEMONS_API);
-            const data = await resp.json();
-            return data;
-        } catch(e) {
-            console.log('Oh no! Something in fetchPokemon went wrong:(', e);
-        }
+  get = async () => {
+    try {
+      const resp = await fetch(POKEMONS_API);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.log("Oh no! Something in fetchPokemon went wrong:(", e);
     }
+  };
 
-    put = async (pokemon) => {
-        try {
-            const resp = await fetch(`${POKEMONS_API}/${pokemon.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(pokemon)
-            });
-            return await resp.json();
-        } catch(e) {
-            console.log('Oh no! Something in updatePokemon went wrong:(', e);
-        }
+  put = async (pokemon) => {
+    try {
+      const resp = await fetch(`${POKEMONS_API}/${pokemon.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pokemon),
+      });
+      return await resp.json();
+    } catch (e) {
+      console.log("Oh no! Something in updatePokemon went wrong:(", e);
     }
+  };
 
+  post = async (newPokemon) => {
+    try {
+      const resp = await fetch(`${POKEMONS_API}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: newPokemon.name,
+          type: newPokemon.type,
+          abilities: [...newPokemon.abilities, newPokemon.ability],
+        }),
+      });
+      const data = await resp.json();
+      console.log(data);
+    } catch (e) {
+      console.log("Oh no! Pokemon counldnt be created.");
+    }
+  };
 }
 
 export const pokemonsApi = new PokemonsApi();
