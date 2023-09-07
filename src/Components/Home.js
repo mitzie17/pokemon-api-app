16511bd4fcb169adaa6c2c673021b87ba1589fe3
Mitzi17 <m_estrada17@live.com>
2023-09-06 22:56:27 -0700

    removed home text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,10 +5,10 @@ import Image from "react-bootstrap/Image";
 export const Home = () => {
   return (
     <div className="home-div">
-      Home
       <Image
         src="https://mir-s3-cdn-cf.behance.net/projects/404/82d2c5174192145.Y3JvcCw5MTYsNzE3LDIwLDA.png"
         roundedCircle
+        className="border border-danger"
       />
     </div>
   );
