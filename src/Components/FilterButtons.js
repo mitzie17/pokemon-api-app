aa612347bc413a2be7ac5b70ce941170b53a07ed
Mitzi17 <m_estrada17@live.com>
2023-09-20 22:49:44 -0700

    removed comments

--- src/Components/FilterButtons.js
+++ src/Components/FilterButtons.js
@@ -1,20 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { pokemonsApi } from "../Api/PokemonsApi";
-import Button from "react-bootstrap/Button";
+import React from "react";
 
 export const FilterButtons = (props) => {
-  const { typesButtons, pokemons, handleTypeBtn, buttons } = props;
-
-  // fetchPokemonTypes = async () => {
-  //   const pokemons = await pokemonsApi.get();
-  //   this.setState({ pokemons });
-  //   console.log(pokemons);
-  //   const filteredPokemon = [
-  //     ...new Set(pokemons.map((pokemon) => pokemon.type)),
-  //   ];
-  //   console.log(filteredPokemon);
-  //   return filteredPokemon;
-  // };
+  const { handleTypeBtn, buttons } = props;
 
   return (
     <div className="filter-buttons">
@@ -26,18 +13,6 @@ export const FilterButtons = (props) => {
           </button>
         );
       })}
-      {/* <div className="d-flex justify-content-center">
-        {typesButtons.map((buttonType, index) => (
-          <button
-            className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
-            value={buttonType}
-            onClick={props.handleTypeBtn}
-            key={index}
-          >
-            {buttonType}
-          </button>
-        ))}
-      </div> */}
     </div>
   );
 };
