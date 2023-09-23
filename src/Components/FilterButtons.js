2691e40795ee955416c1330fcee16e3e2c6deb8b
Mitzi17 <m_estrada17@live.com>
2023-09-22 21:10:22 -0700

    color change filter buttons based on type

--- src/Components/FilterButtons.js
+++ src/Components/FilterButtons.js
@@ -1,18 +1,44 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export const FilterButtons = (props) => {
   const { handleTypeBtn, buttons } = props;
 
+  const buttonColorChange = (button) => {
+    if (button === "Water") {
+      return "primary";
+    } else if (button === "Fire") {
+      return "danger";
+    } else if (button === "Grass") {
+      return "success";
+    } else if (button === "Electric") {
+      return "warning";
+    } else if (button === "Ice") {
+      return "info";
+    } else if (button === "Normal") {
+      return "secondary";
+    } else {
+      return "dark";
+    }
+  };
+
   return (
-    <div className="filter-buttons">
-      Filter by:
-      {props.buttons.map((button, index) => {
-        return (
-          <button key={index} value={button} onClick={props.handleTypeBtn}>
-            {button}
-          </button>
-        );
-      })}
+    <div className="container-buttons">
+      <h5>Filter by type:</h5>
+      <div className="type-buttons">
+        {props.buttons.map((button, index) => {
+          return (
+            <Button
+              variant={buttonColorChange(button)}
+              key={index}
+              value={button}
+              onClick={props.handleTypeBtn}
+            >
+              {button}
+            </Button>
+          );
+        })}
+      </div>
     </div>
   );
 };
